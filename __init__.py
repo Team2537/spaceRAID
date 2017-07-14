3c0083e4c4bdb05d9a74b09524483ed7c2c6eaaa
TheGreatKetchup <TheGreatKetchup@users.noreply.github.com>
2017-07-14 10:03:58 -0400

    Removed files by adding them to .gitignore.

--- __init__.py
+++
@@ -1,4 +0,0 @@
-"""
-Takes an video file (very long) and slowly pulls the matches in the video out
-and makes those seperate files.
-"""
--- find_matches.py
+++
@@ -1,4 +0,0 @@
-"""Takes the video feed and looks for signs that a match is there."""
-MATCH_LENGTH = 188 # seconds (can be different with weird matches)
-
-# Take a movie and 
