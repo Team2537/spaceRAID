12b0fbc68883d7fd67fcc553f808cc7a59018c78
TheGreatKetchup <TheGreatKetchup@users.noreply.github.com>
2017-11-18 22:04:01 -0500

    Finished the argparse arguments. Still needs to be integrated into the program and tested.

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 """
 Takes an video file (very long) and slowly pulls the matches in the video out
 and makes those seperate files.
@@ -5,8 +6,9 @@ and makes those seperate files.
 Various possibilites and operations.
 test, parse, finish, upload, run
 
-spaceraid (run|parse|finish|upload|test) <input file> ... <output file/folder>
+spaceraid {run,parse,finish,upload,test} source_file ... target_dir
 (global)
+--help
 --version             Print version info and exit.
 -v --verbose          Turn on verbose reporting.
 -l --log file         Logging file.
@@ -28,64 +30,207 @@ finish    Fixup the output video files. Add intro and fix name.
 
 upload    Upload the file to youtube.
 run       Do parse, finish, and probably upload. Run will be done if no
-          command is given.
+          command is given if I can figure out how to add that.
 """
-# Configure logging.
+__version__ = "1.0"
+
+import sys
 import logging
+import argparse
 
 logging.basicConfig(
     format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 
-import sys
-import argparse
+import find_matches
 
-parser = argparse.ArgumentParser()
+# ==============
+# Type classes
+# PathType
+# This is meant to allow for verifcation the passed argument is a path, but
+# not opening a file immediatly.
+# This can be useful for multiple things.
+# - 1) In case of error, such as from a file that is read in, the program can
+#      exist without having ever created an output file.
+# - 2) Can be used to specify
+# ==============
 
-##parser.add_argument('input_files', metavar='infile', type=int, nargs='+',
-##                    help='an integer for the accumulator')
+# Code taken from https://stackoverflow.com/questions/11415570/directory-path-types-with-argparse
+# This is EXACTLY what I was going to build otherwise.
+# This is also suggested to be an official change to argparse.
+# https://mail.python.org/pipermail/stdlib-sig/2015-July/000990.html
 
-##parser.add_argument('--log-lvl', action='store', dest = 'logging_level',
-##                    choices = ('CRITICAL', 'DEBUG', 'ERROR', 'FATAL', 'INFO',
-##                               'NOTSET', 'WARN', 'WARNING', "NONE"),
-##                    default = "DEBUG", help = "Change the logging level of data shown.")
+# Added callable function, instead of __call__ check, and changed err to
+# ArgumentTypeError. Also fixed os.path.sympath reference to os.path.islink.
+import os
+from argparse import ArgumentTypeError
 
-parser.add_argument("-v", "--verbose",
-                    help="increase output verbosity", action="store_true")
+class PathType(object):
+    def __init__(self, exists=True, type='file', dash_ok=True):
+        '''exists:
+                True: a path that does exist
+                False: a path that does not exist, in a valid parent directory
+                None: don't care
+           type: file, dir, symlink, None, or a function returning True for valid paths
+                None: don't care
+           dash_ok: whether to allow "-" as stdin/stdout'''
 
-parser.add_argument('--version', action='version', version='%(prog)s 1.0')
+        assert exists in (True, False, None)
+        assert type in ('file','dir','symlink',None) or callable(type)
+
+        self._exists = exists
+        self._type = type
+        self._dash_ok = dash_ok
+
+    def __call__(self, string):
+        if string=='-':
+            # the special argument "-" means sys.std{in,out}
+            if self._type == 'dir':
+                raise ArgumentTypeError('standard input/output (-) not allowed as directory path')
+            elif self._type == 'symlink':
+                raise ArgumentTypeError('standard input/output (-) not allowed as symlink path')
+            elif not self._dash_ok:
+                raise ArgumentTypeError('standard input/output (-) not allowed')
+            return string # No reason to check anything else if this works.
+
+        exists = os.path.exists(string)
+        if self._exists == True:
+            if not exists:
+                raise ArgumentTypeError("path does not exist: '%s'" % string)
+
+            if self._type is None:
+                pass
+            elif self._type=='file':
+                if not os.path.isfile(string):
+                    raise ArgumentTypeError("path is not a file: '%s'" % string)
+            elif self._type=='symlink':
+                if not os.path.islink(string):
+                    raise ArgumentTypeError("path is not a symlink: '%s'" % string)
+            elif self._type=='dir':
+                if not os.path.isdir(string):
+                    raise ArgumentTypeError("path is not a directory: '%s'" % string)
+            elif not self._type(string):
+                raise ArgumentTypeError("path not valid: '%s'" % string)
+        else:
+            if self._exists == False and exists:
+                raise ArgumentTypeError("path exists: '%s'" % string)
+
+            p = os.path.dirname(os.path.normpath(string)) or '.'
+            if not os.path.isdir(p):
+                raise ArgumentTypeError("parent path is not a directory: '%s'" % p)
+            elif not os.path.exists(p):
+                raise ArgumentTypeError("parent directory does not exist: '%s'" % p)
+
+        return string
+
+############################### Parser ###############################
+parser = argparse.ArgumentParser(prog = "spaceraid")
+
+parser.add_argument("-q", "--quiet", dest = "log_level", action = "store_const",
+                    const = logging.ERROR, help="Make the output less verbose"
+                    "with only important information. (Default).")
+
+parser.add_argument("-v", "--verbose", dest = "log_level", action="store_const",
+                    const = logging.DEBUG, help="Make the output verbose.")
+
+parser.set_defaults(log_level = logging.ERROR)
+
+parser.add_argument("--version", action="version", version="%(prog)s " + __version__)
+
+parser.add_argument("-d", "--dryrun", action = "store_true", default = False,
+                    help = "Don't read any files or access the internet. "
+                           "List what would be done.")
+
+parser.add_argument("-l","--log","--log_file",type=PathType(exists=None),default='-',
+                    help = "Logging file for all debug info from logging.")
 
 # Subparsers / Operations
 subparsers = parser.add_subparsers(
-    dest = "operation", title = "Operations", metavar = "operation",
+    dest = "operation_name", title = "Operations",
     description = "Processing operation to be taken.",
-    help="Commands for spaceraid. Run is default.")
+    help="Commands for %(prog)s. Run is default.")
 
-default = subparsers.add_parser("")
-parser_run = subparsers.add_parser("run", help = (
-    "Do parse, finish, and probably upload. Run will be done if no command "
-    "is given."))
+################################ Run #################################
+parser_run = subparsers.add_parser("run", help = \
+    "Do parse, finish, and upload. Run will be done if no command is given.")
 
-# This is the default.
+# This should be the default. I just have to
+# figured out how to do that.
 ##subparsers.default = parser_run
 ##parser.set_defaults(operation = parser_run)
+##default = subparsers.add_parser("")
 
-parser_parse = subparsers.add_parser("parse", help = "Analyize the video(s).")
+def run(namespace):
+    """Run operation for spaceraid."""
+    raise NotImplementedError("Haven't run finish yet.")
+
+parser_run.set_defaults(operation = run)
+
+del parser_run # No need to keep varible.
+############################### Parse ################################
+parser_parse = subparsers.add_parser("parse", help = "Analyze the video(s).")
+
+def parse(namespace):
+    """Parse operation for spaceraid."""
+    raise NotImplementedError("Haven't made parse yet.")
+
+parser_parse.set_defaults(operation = parse)
+
+del parser_parse # No need to keep varible.
+############################### Finish ###############################
+# -t --tags [all|yellow|green]                                       #
+#     Look at the eXtra tags and only finish tags tagged with        #
+#     green and not red. Default is green. Yellow is will            #
+#     process videos with yellow flags but not green.                #
 
 parser_finish = subparsers.add_parser("finish", help = (
     "Fixup the output video files. Add intro and fix name."))
 
+parser_finish.add_argument(
+    "-t","--tags",choices=("all","yellow","green"),default = "green",
+    help = ("Look at the eXtra tags and only finish tags tagged with"
+            "green and not red. Default is green. Yellow is will"
+            "process videos with yellow flags but not green."))
+
+def finish(namespace):
+    """Finish operation for spaceraid."""
+    raise NotImplementedError("Haven't made finish yet.")
+
+parser_finish.set_defaults(operation = finish)
+del parser_finish # No need to keep varible.
+################################ Run #################################
 parser_upload=subparsers.add_parser("upload",help="Upload the file to youtube.")
 
+def upload(namespace):
+    """Upload operation for spaceraid."""
+    raise NotImplementedError("Haven't made upload yet.")
+
+parser_upload.set_defaults(operation = upload)
+del parser_upload # No need to keep varible.
+################################ Test ################################
 parser_test = subparsers.add_parser("test", help = (
     "Run a test to see if the video is readable."))
 
-parser.add_argument('infile', nargs='+', type=argparse.FileType('r'),
-                    default=sys.stdin)
+###This would require more effort to implement than I think is worth.
+##parser_test.add_argument("-i", "--image_folder", type = PathType(type='dir'))
 
-parser.add_argument('outfile', type=argparse.FileType('w'))
+parser_test.add_argument("-a", "--answers", dest = "answers_file",
+                         type = PathType(), help =
+                         "Actual results for the video so they "
+                         "can be compared to. This should be a "
+                         "cvs file with frame #, name, time.")
+
+def test(namespace):
+    """Test operation for spaceraid."""
+    raise NotImplementedError("Haven't made test yet.")
+
+parser_test.set_defaults(operation = test)
+del parser_test
+######################################################################
+parser.add_argument('source_file', nargs='+', action = "append", type= PathType(),
+                    help = "Video file to analyze.")
+
+parser.add_argument('target_dir', type = PathType(type='dir', exists = None),
+                    help = "Output folder for processed videos.")
 
 results = parser.parse_args()
-
-from pprint import pprint
-
-pprint(results)
+print(results)
