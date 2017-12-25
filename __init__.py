e71961ac6886a416084d192d9eece9374a8de32e
TheGreatKetchup <TheGreatKetchup@users.noreply.github.com>
2017-12-24 21:34:34 -0500

    Updated __init__.py verion.

--- __init__.py
+++ __init__.py
@@ -32,7 +32,7 @@ upload    Upload the file to youtube.
 run       Do parse, finish, and probably upload. Run will be done if no
           command is given if I can figure out how to add that.
 """
-__version__ = "1.0"
+__version__ = "1.1"
 
 # WARNING, this is not stable unless the current directory is the directory
 # with this file.
